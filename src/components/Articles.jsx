748c8e14c93df99dca346f4a3b652f8b001c0c48
Christos Kaitatzis <xrkaitatzis@gmail.com>
2026-06-05 19:06:04 +0300

    Update Articles.jsx

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -17,7 +17,7 @@ export default function Articles() {
 
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
           {ARTICLES.map((article) => (
-            
+            <a 
               key={article.url}
               href={article.url}
               target="_blank"
